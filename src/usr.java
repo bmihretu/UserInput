13266ba9ae9bbfe841c97b94b701f1f6f2e1157e
bereket <motta4259@gmail.com>
2019-06-12 15:24:15 -0400

    first commit

---
+++ src/usr.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class usr{
+    public static void main(String[] args){
+
+        String line;
+        String firstInitial, lastName, streetName, streetType, city;
+        String name = "P Sherman 42 Wallaby Way Sydney";
+        int houseNum;
+        System.out.print("P Sherman 42 Wallaby Way Sydney");
+    }//main
+}//class
